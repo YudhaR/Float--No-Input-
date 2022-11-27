f1cb022c79a9011da68a50b85dcc2e5d7b29911f
YudhaR <yudharizqia182@gmail.com>
2022-11-28 04:31:41 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/*
+    Program Float
+    Ket : Program Float
+    By  : Yudha Rizqia Grafer
+    Tgl : 14 September 2022
+
+*/
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    float c;
+    a = 10;
+    b = 5;
+    c = float ( a + b )/2;
+    cout << "Isi variabel a = " << a << endl;
+    cout << "Isi variabel b = " << b << endl;
+    cout << "Isi variabel c = " << c << endl;
+    return 0;
+}
